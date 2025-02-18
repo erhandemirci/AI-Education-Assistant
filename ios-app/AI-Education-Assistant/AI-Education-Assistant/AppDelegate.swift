b19990a12c14de1fdf612d68d5bd7ab5d75b7549
erhan <erhandemirci2@gmail.com>
2025-02-18 23:33:47 +0300

    project folders created . Also ios app created

---
+++ ios-app/AI-Education-Assistant/AI-Education-Assistant/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  AI-Education-Assistant
+//
+//  Created by ios developer  on 18/2/2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ ios-app/AI-Education-Assistant/AI-Education-Assistant/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AI-Education-Assistant
+//
+//  Created by ios developer  on 18/2/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
